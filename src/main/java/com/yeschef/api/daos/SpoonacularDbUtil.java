5c7160224b2fa9643e4fc18a83106c921bfd0d32
Dylan Yelenich <131614633+dylsey@users.noreply.github.com>
2024-02-15 18:07:36 -0600

    Update SpoonacularDbUtil.java
    removed personal api key from public repo

--- src/main/java/com/yeschef/api/daos/SpoonacularDbUtil.java
+++ src/main/java/com/yeschef/api/daos/SpoonacularDbUtil.java
@@ -6,7 +6,7 @@ public class SpoonacularDbUtil {
 
 	private static String connectionUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com";
 	private static String header1 = "x-rapidapi-key";
-	private static String apiKey = "e033f0fec8msh8822ae38ed8d4c7p1b7c6cjsn6d220a7560f2";
+	private static String apiKey = "****user specific API key****";
 	private static String header2 = "x-rapidapi-host";
 	private static String hostKey = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com";
 	
